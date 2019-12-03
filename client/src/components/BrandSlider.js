0af94dc7d7f9c832e964675d2bdba6a6e76c8f0f
Jasper Khonesavanh <jasperkhonesavanh@gmail.com>
2019-12-03 15:41:06 -0800

    changes to main

--- client/src/components/BrandSlider.js
+++ client/src/components/BrandSlider.js
@@ -1,35 +1,20 @@
 import React from "react"
-import ReactDom from "react-dom"
-import {brandImages} from "./BrandSliderImages"
-import {Gallery, GalleryImage} from 'react-gesture-gallery'
+import { brandImages } from "./BrandSliderImages"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 import "../styles/base.css"
-
-const START_INDEX = 0
+import { Link } from "react-router-dom"
 
 export default function() {
-   const [index, setIndex] = React.useState(START_INDEX)
-   
+
     return (
-        <Gallery
-        index={index}
-        enableControls={true}
-        enableIndicators={false}
-        onRequestChange={i => {
-            setIndex(i)
-        }}
-        >
-            {brandImages.map((image, i) => (
-                <div key={"brandslider" + i} className="brandCSS">
-                 <a href={image.url1}><GalleryImage className="bImage" objectFit="cover" src={image.img1}/></a>
-                 <a href={image.url2}><GalleryImage className="bImage" objectFit="cover" src={image.img2}/></a>
-                 <a href={image.url3}><GalleryImage className="bImage" objectFit="cover" src={image.img3}/></a>
-                 <a href={image.url4}><GalleryImage className="bImage" objectFit="cover" src={image.img4}/></a>
-                 <a href={image.url5}><GalleryImage className="bImage" objectFit="cover" src={image.img5}/></a>
-                 <a href={image.url6}><GalleryImage className="bImage" objectFit="cover" src={image.img6}/></a>
-                 <a href={image.url7}><GalleryImage className="bImage" objectFit="cover" src={image.img7}/></a>
-                 <a href={image.url8}><GalleryImage className="bImage" objectFit="cover" src={image.img8}/></a>
+        <div className="brandshome">
+            {brandImages.map((brand, i) => (
+                <div key={"brand" + i}>
+                    <Link to={brand.url}>
+                        <img src={`${brand.img}`} alt="whoops" />
+                    </Link>
                 </div>
             ))}
-        </Gallery>
+        </div>
     )
 }
