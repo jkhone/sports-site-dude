4771aa9c6a2e0fe4858fa1c2709cebaffaa8ba5c
Alexander Gonzalez <julian1alex@gmail.com>
2019-12-02 18:41:09 -0800

    main gallery

---
+++ client/src/components/homesliderimages.js
@@ -0,0 +1,30 @@
+
+export const popImages = [
+    {
+    img: "https://images.unsplash.com/photo-1559666126-84f389727b9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1356&q=80",
+    url: "/player/1"
+
+    },
+    {
+    img: "https://images.unsplash.com/photo-1559666126-84f389727b9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1356&q=80",
+    url: "/player/2"
+
+    },
+    {
+    img: "https://images.unsplash.com/photo-1559666126-84f389727b9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1356&q=80",
+    url: "/player/3"
+
+    },
+    {
+    img: "https://images.unsplash.com/photo-1559666126-84f389727b9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1356&q=80",
+    url: "/player/4"
+
+    },
+    {
+    img: "https://images.unsplash.com/photo-1559666126-84f389727b9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1356&q=80",
+    url: "/player/5"
+        
+    }
+
+  ];
+
