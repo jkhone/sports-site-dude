84c98bf55da407add20e60d86008e098bb23f79e
Kaden Neuman <kadenn2019@gmail.com>
2019-12-03 16:18:49 -0800

    fixed conflicts

--- client/src/components/BrandSlider.js
+++ client/src/components/BrandSlider.js
@@ -1,11 +1,6 @@
 import React from "react"
-<<<<<<< HEAD
-import {brandImages} from "./BrandSliderImages"
-import {Gallery, GalleryImage} from 'react-gesture-gallery'
-=======
 import { brandImages } from "./BrandSliderImages"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
->>>>>>> 0af94dc7d7f9c832e964675d2bdba6a6e76c8f0f
 import "../styles/base.css"
 import { Link } from "react-router-dom"
 
