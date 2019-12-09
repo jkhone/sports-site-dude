5a2bf6999a2c64c0189d35e01dae9eef3d2b80b7
Jasper Khonesavanh <jasperkhonesavanh@gmail.com>
2019-12-09 14:49:00 -0800

    working properly

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -14,15 +14,15 @@ export default props => {
         <div><PlayerSearch /></div>
     
     <div id="playercontainer">
-    
       {players.map((players, i) => (
       <div id="profile"  key={'player' + i} >
-      <Link to={"/player/" + players.id}>
-        <div className="player">
-          <img className="playerImg" src={`${players.url}`} alt="" />
-          <div className="playerDesc">
-            <div className="teamName">{players.team}</div>
-            <div className="playerName">{players.player}</div>
+        <Link to={"/player/" + players.id}>
+          <div className="player">
+            <img className="playerImg" src={`${players.url}`} alt="" />
+            <div className="playerDesc">
+              <div className="teamName">{players.team}</div>
+              <div className="playerName">{players.player}</div>
+            </div>
           </div>
         </Link>
   	  </div>
