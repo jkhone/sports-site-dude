6a32d993e0541b2c7329782a32cf229e8ac3cbf0
Jasper Khonesavanh <jasperkhonesavanh@gmail.com>
2019-12-13 15:10:55 -0800

    stars and stuff

--- client/src/components/ProductPage.js
+++ client/src/components/ProductPage.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import Popup from "reactjs-popup";
 import Image from 'react-bootstrap/Image'
 import "../styles/ProductPage.css"
+import Rating from "react-rating"
 
 export default props => {
     const [shoe, setShoe] = useState([])
@@ -31,6 +32,16 @@ export default props => {
         })
     }, [id])
 
+    function getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    }
+
     return (
         <div className="page">
             <div className='product'>
@@ -42,6 +53,15 @@ export default props => {
                     <div className="name">{shoe.shoe}</div>
                     <div className="size">Size {shoe.size}</div>
                     <div className="ShoePrice">${shoe.price}</div>
+                    <div className="ratings" >
+                        <Rating
+                        initialRating={getRandomArbitrary(2.5, 5)}
+                        readonly
+                        emptySymbol="fa fa-star-o fa-2x"
+                        fullSymbol="fa fa-star fa-2x"
+                        />
+                        <p>({getRandomInt(100, 1000)})</p>
+                    </div>
                     <button className="addToCart" onClick={e => handleAdd(e, shoe)}>Add to Cart</button>
                     <Popup trigger={<button className="puButton"> Who wears this shoe?</button>} position="right center">
                         <div>
